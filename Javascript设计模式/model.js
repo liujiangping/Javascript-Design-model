9ed43e31fb789596e6b30e5ec1335cd5faca1c25
ljp <765032776@qq.com>
2021-05-30 20:54:21 +0800

    no message

--- Javascript设计模式/model.js
+++ Javascript设计模式/model.js
@@ -18,8 +18,6 @@ var testModule = (function () {
   };
 })();
 
-
-// 带命名空间
 var myNamespace = (function () {
   var myPrivateVar, myPrivateMethod;
 
@@ -36,6 +34,48 @@ var myNamespace = (function () {
       myPrivateVar++;
 
       myPrivateMethod(bar);
+    },
+  };
+})();
+
+var basketModule = (function () {
+  // privates
+
+  var basket = [];
+
+  function doSomethingPrivate() {
+    //...
+  }
+
+  function doSomethingElsePrivate() {
+    //...
+  }
+
+  // Return an object exposed to the public
+  return {
+    // Add items to our basket
+    addItem: function (values) {
+      basket.push(values);
+    },
+
+    // Get the count of items in the basket
+    getItemCount: function () {
+      return basket.length;
+    },
+
+    // Public alias to a  private function
+    doSomething: doSomethingPrivate,
+
+    // Get the total value of items in the basket
+    getTotal: function () {
+      var q = this.getItemCount(),
+        p = 0;
+
+      while (q--) {
+        p += basket[q].price;
+      }
+
+      return p;
     },
   };
 })();
